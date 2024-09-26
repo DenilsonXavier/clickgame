4b1f621cb84bb379d031cc53ef79542b9a7fd1a5
Gustavolmg <Gustavolmgomes@gmail.com>
2024-09-25 21:34:55 -0300

    fix the tooltip

--- src/Updates.jsx
+++ src/Updates.jsx
@@ -1,89 +1,25 @@
 import { ItemCellUpgrades } from "./ItemCell";
-
+import { useState } from "react";
+import * as itemsjson from "./items.json";
 function Updates() {
+  const [itemUpgrades, setitemUpgrades] = useState(
+    localStorage.getItem("itemUpgrades")
+      ? JSON.parse(localStorage.getItem("itemUpgrades"))
+      : Object.values(itemsjson["itemUpgrades"])
+  );
+
   return (
     <div className="items">
-      <ItemCellUpgrades
-        ItemId={11}
-        ItemImg="upgradeImg1"
-        ItemName="Upgrade de Produção"
-        ItemPrice={500}
-        ItemLevel={0}
-        ItemProduction={5}
-        ItemDescription=""
-      />
-      <ItemCellUpgrades
-        ItemId={12}
-        ItemImg="upgradeImg2"
-        ItemName="Melhoria de Eficiência"
-        ItemPrice={1000}
-        ItemLevel={0}
-        ItemProduction={10}
-      />
-      <ItemCellUpgrades
-        ItemId={13}
-        ItemImg="upgradeImg3"
-        ItemName="Melhoria de Eficiência"
-        ItemPrice={1000}
-        ItemLevel={0}
-        ItemProduction={10}
-      />
-      <ItemCellUpgrades
-        ItemId={14}
-        ItemImg="upgradeImg4"
-        ItemName="Melhoria de Eficiência"
-        ItemPrice={1000}
-        ItemLevel={0}
-        ItemProduction={10}
-      />
-      <ItemCellUpgrades
-        ItemId={15}
-        ItemImg="upgradeImg5"
-        ItemName="Melhoria de Eficiência"
-        ItemPrice={1000}
-        ItemLevel={0}
-        ItemProduction={10}
-      />
-      <ItemCellUpgrades
-        ItemId={16}
-        ItemImg="upgradeImg6"
-        ItemName="Melhoria de Eficiência"
-        ItemPrice={1000}
-        ItemLevel={0}
-        ItemProduction={10}
-      />
-      <ItemCellUpgrades
-        ItemId={17}
-        ItemImg="upgradeImg7"
-        ItemName="Melhoria de Eficiência"
-        ItemPrice={1000}
-        ItemLevel={0}
-        ItemProduction={10}
-      />
-      <ItemCellUpgrades
-        ItemId={18}
-        ItemImg="upgradeImg8"
-        ItemName="Melhoria de Eficiência"
-        ItemPrice={1000}
-        ItemLevel={0}
-        ItemProduction={10}
-      />
-      <ItemCellUpgrades
-        ItemId={19}
-        ItemImg="upgradeImg9"
-        ItemName="Melhoria de Eficiência"
-        ItemPrice={1000}
-        ItemLevel={0}
-        ItemProduction={10}
-      />
-      <ItemCellUpgrades
-        ItemId={20}
-        ItemImg="upgradeImg10"
-        ItemName="Melhoria de Eficiência"
-        ItemPrice={1000}
-        ItemLevel={0}
-        ItemProduction={10}
-      />
+      {itemUpgrades.map((items, index) => (
+        <ItemCellUpgrades
+          ItemId={index}
+          ItemImg={items.img}
+          ItemName={items.name}
+          ItemSold={items.sold}
+          ItemPrice={items.price}
+          ItemDescription={items.description}
+        />
+      ))}
     </div>
   );
 }
